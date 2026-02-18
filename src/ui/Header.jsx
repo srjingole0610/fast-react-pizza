88efc5ac646ca0312d83b0235d05ee5622d5abe6
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2026-02-19 00:28:37 +0530

    feat: enhance responsive styling in CartOverview, Header, and Home components

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/users/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-500">
+    <header className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-500 sm:px-6">
       <Link to="/" className=" tracking-[0.25rem]">
         Fast React Pizza Co.
       </Link>
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,7 +2,7 @@ import CreateUser from "../features/users/CreateUser";
 
 function Home() {
   return (
-    <div className="my-10 text-center">
+    <div className="my-1 sm:my-16 text-center">
       <h1 className=" text-2xl font-semibold mb-12 ">
         The best pizza.
         <br />
